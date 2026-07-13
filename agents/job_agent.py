82c4cc24fb3bab832456027d445df931dbbe2a1f
kiran566 <kiranbasava230@gmail.com>
2026-07-13 10:25:42 +0530

    done of using mcp-adapters which will load mcp tools as langchain tools aftr client and server session

---
+++ agents/job_agent.py
@@ -0,0 +1,56 @@
+import asyncio
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+
+from langchain_groq import ChatGroq
+# from langchain_openai import ChatOpenAI
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# SERVER
+server_params = StdioServerParameters( command="python", args=["mcp_servers/search_server.py"], )
+
+
+async def main():
+
+    async with stdio_client(server_params) as (read, write):
+
+        async with ClientSession(read, write) as session:
+
+            await session.initialize()
+
+            # Load MCP tools
+            tools = await load_mcp_tools(session)
+
+            # Create LLM
+            llm = ChatGroq(
+                  model="llama-3.3-70b-versatile"
+            )
+            # Create agent
+            agent = create_agent(
+                model=llm,
+                tools=tools
+            )
+
+            # Invoke agent
+            response = await agent.ainvoke(
+                {
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": "Find Prompt Engineer jobs"
+                        }
+                    ]
+                }
+            )
+
+            print(response)
+    
+if __name__ == "__main__":
+    asyncio.run(main())
